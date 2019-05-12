e129a26c045980daba6fe15c3276b7f3df25e431
CHNxish <1095219764@qq.com>
2019-05-12 18:00:38 +0800

    Update Chapter3.c

--- Basics/Pointers on c/Chapter3.c
+++ Basics/Pointers on c/Chapter3.c
@@ -8,10 +8,13 @@ signed unsigned
 八进制整型：0123
 十六进制整型：0x123
 2）浮点型
+float double long double
 3）指针
 4）聚合类型
+enum union struct
 
 limits.h：规定了整型的最大值和最小值
+float.h:规定了浮点型的最大值和最小值
 stdbool.h:bool true false
 complex.h:complex imaginary 配合 double,float,long double使用 I：虚数宏 creal() cimag()：计算实部和虚部，输出时还要带%+.1fi，
 不自带i这个符号
@@ -21,4 +24,20 @@ wchar.h:wchar_t wscanf wprintf wchar_t wstr[]=L"something"; wprintf(L"%ls\n",wst
 在使用wscanf等函数直接必须用setlocale设置本地环境，设置完成后，不能再使用scanf函数，必须使用wscanf函数
 ！！！wchar_t只能在linux中使用，不能再windows中使用，因为linux中wchar_t占4个字节，在windows中占2个字节
 ！！！在windows使用中文可以使用tchar.h DWORD TEXT("something");
+
+#define 宏定义
+typedef 类型定义
+
+修饰符
+const static
+
+作用域：1）代码块（局部）作用域
+2）文件（全局）作用域
+3）原型（函数参数）作用域
+4）函数作用域：goto语句，不属于局部，不属于全局，不属于原型
+
+链接属性：1）外部（external）
+2）内部（internal）
+3）无（none）
+链接属性指标识符，而不仅仅是变量和函数，还有typedef int *a; 此时a是无属性标识符
 */
