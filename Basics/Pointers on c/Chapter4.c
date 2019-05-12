bea12ebd8348434cc4bbd0baab9c8d6f8abe74a9
CHNxish <1095219764@qq.com>
2019-05-12 21:43:18 +0800

    Create Chapter4.c

---
+++ Basics/Pointers on c/Chapter4.c
@@ -0,0 +1,47 @@
+/*
+代码块
+{
+  declarations
+  statements
+}
+
+if(){
+
+}
+else if(){
+
+}
+else{
+
+}
+
+while(){
+
+}
+
+do{
+
+}while();
+
+for(;;){
+
+}
+
+switch(){
+  case :
+    break;
+  case :
+    break;
+  default:
+    break;
+}
+
+break
+continue
+
+xxx:
+*/
+code
+/*
+goto xxx;
+*/
