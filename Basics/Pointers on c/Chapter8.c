f5191d790e9a963c0ce2a5ed1e3d0820239ad36e
CHNxish <1095219764@qq.com>
2019-05-17 23:40:41 +0800

    Update Chapter8.c

--- Basics/Pointers on c/Chapter8.c
+++ Basics/Pointers on c/Chapter8.c
@@ -19,4 +19,40 @@ void try5(){
 2）声明在寄存器中的变量的指针要比位于静态内存或者堆栈中的指针效率更高（具体提高的幅度取决于你使用的机器）
 3）如果你可以在一个循环中不是用计数器，而使用其他的方法终止循环，你应该不用计数器
 最总要的一点：时间换空间，空间换时间，当真的没有办法避免的时候，这个是最有效的方法
+
+八皇后问题：
+##
+
+#include <stdio.h>
+#define QUEENS     8
+#define IS_OUTPUT  1
+
+int A[QUEENS+1],B[QUEENS*3+1],C[QUEENS*3+1],K[QUEENS+1][QUEENS+1];
+int inc,*a=A,*b=B+QUEENS,*c=C,*k=K;
+void lay(int row){
+  int column=0,i,j;
+  
+  while(++column<QUEENS){
+    if(a[column]+b[column-row]+c[column+row]==0){
+      k[row][column]=a[column]=b[column-row]=c[column+row]=1;
+      if(i<QUEENS) lay(i+1);
+      else{
+        ++inc;
+        if(IS_OUTPUT){
+          for(printf("(%d)\n",inc),i=QUEENS+1;--i;printf("\n"))
+            for(j=QUEENS+1;--j;) k[i][j]?printf("Q "):printf("+ ");
+          printf("\n\n\n");
+        }
+      }
+      k[row][column]=a[column]=b[column-row]=c[column+row]=0;
+  }
+}
+
+int main(void){
+  lay(1);
+  printf("%d皇后共计%d个解\n",QUEENS,inc);
+  return 0;
+}
+
+##
 */
