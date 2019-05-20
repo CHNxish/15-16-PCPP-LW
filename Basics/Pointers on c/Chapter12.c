4f7f83ae86e63a233bf78f7d7162bb8f955b8824
CHNxish <1095219764@qq.com>
2019-05-20 16:55:01 +0800

    Update Chapter12.c

--- Basics/Pointers on c/Chapter12.c
+++ Basics/Pointers on c/Chapter12.c
@@ -1,3 +1,39 @@
 /*
+typedef struct node{
+  struct node *next;
+  int value;
+}Node;
+
+##insert2.c
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "sll_node.h"
+
+int sll_insert(Node **rootp,int new_value){
+  Node *previous;
+  Node *current;
+  Node *new;
+  
+  previous=NULL;
+  current=*rootp;
+  while(current!=NULL && current->value<new_value){
+    previous=current;
+    current=current->link;
+  }
+  
+  new=(Node *)malloc(sizeof(Node));
+  if(new==NULL)
+    return false;
+  new->value=new_value;
+  
+  new->link=current;
+  if(previous==NULL)
+    *rootp=new;
+  else
+    previous->link=new;
+   return true;
+}
+##
 
 */
