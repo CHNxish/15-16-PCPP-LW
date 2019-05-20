538b571f977e80643a3b0041d0e2cb84853ad684
CHNxish <1095219764@qq.com>
2019-05-20 13:19:03 +0800

    Update Chapter11.c

--- Basics/Pointers on c/Chapter11.c
+++ Basics/Pointers on c/Chapter11.c
@@ -13,7 +13,7 @@ if(pi==NULL){
 所有执行程序共享一个通用内存池的系统，内存泄漏将会一点点榨干可用内存，最终将一无所有
 摆脱这个困境，只有重启系统
 
-##
+##sort.c
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -58,6 +58,74 @@ int main(){
   free(array);
   return EXIT_SUCCESS;
 }
+
+##inventor.h
+typedef struct{
+  int cost;
+  int supplier;
+}Partinfo;
+
+typedef struct{
+  int n_parts;
+  struct SUBASSYPART{
+    char patrno[10];
+    short quan;
+  }*part;
+}Subassyinfo;
+
+typedef struct{
+  char partno[10];
+  int quan;
+  enum{PART,SUBASSY} type;
+  union{
+    Partinfo *part;
+    Subassyinfo *subassy;
+  }info;
+}Invrec;
+##
+
+##invcreat.c
+#include <stdlib.h>
+#include <stdio.h>
+#include "inventor.h"
+
+Invrec *create_subassy_record(int n_parts){
+  Invrec *new_rec;
+  new_rec=malloc(sizeof(Invrec));
+  if(new_rec!=NULL){
+    new_rec->info.subassy=malloc(sizeof(Subassyinfo));
+    if(new_rec->ifo.subassy!=NULL){
+      new_rec->info.subassy->part=malloc(n_parts*sizeof(struct SUBASSYPART));
+      if(new_rec->info.subassy->part!=NULL){
+        new_rec->type=SUBASSY;
+        new_rec->info.subassy->n_parts=n_parts;
+        return new_rec;
+      }
+      free(new->info.subassy);
+    }
+    free(new_rec);
+  }
+  return NULL;
+}
+##
+
+##invdelet.c
+#include <stdlib.h>
+#include "inventor.h"
+
+void discard_inventory_record(Invrec* record){
+  switch(record->type){
+  case SUBASSY:
+    free(record->info.subassy->part);
+    free(record->info.subassy);
+    break;
+  case PART:
+    free(record->info.part);
+    break;
+  }
+  free(record);
+}
+##
 
 ##
 */
