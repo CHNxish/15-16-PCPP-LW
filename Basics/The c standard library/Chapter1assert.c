a257dbd51065af625e1022958b894373f107acff
CHNxish <1095219764@qq.com>
2019-05-26 19:18:09 +0800

    Update Chapter1assert.c

--- Basics/The c standard library/Chapter1assert.c
+++ Basics/The c standard library/Chapter1assert.c
@@ -31,4 +31,11 @@ void _Assert(char *mesg)
 1）不论是否定义了assert这个宏，先通过undef将这个宏给解除定义
 2）通过ifdef else endif来控制宏定义
 3）##表示将两个字符连接起来，#将符号变成字符串
+4）编译器预编译文件，汇编文件，可执行文件
+gcc test.c -E > test.i
+gcc test.c -S test.s
+gcc test.c -o test
+5）宏处理应该使用的是多次处理，第一次将STR变成VAL，__LINE__变成常量，第二次将VAL宏让常量变成字符串
+6）使用隐式的函数，使用库文件，输出和中断，不应该在头文件内引用头文件
+7）在使用assert.h的时候，先定义NDEBUG在某一段代码不断言，在后面undef NDEBUG再使用assert宏
 * /
