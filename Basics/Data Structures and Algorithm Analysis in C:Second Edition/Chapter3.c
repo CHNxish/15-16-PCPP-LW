f680cc3b0a4b1d6d0fa25aef7bd7badfaac7ea73
CHNxish <1095219764@qq.com>
2019-06-22 15:37:08 +0800

    Update Chapter3.c

--- Basics/Data Structures and Algorithm Analysis in C:Second Edition/Chapter3.c
+++ Basics/Data Structures and Algorithm Analysis in C:Second Edition/Chapter3.c
@@ -19,3 +19,32 @@ struct Node{
 };
 循环链表（A1的Prev和An的Next指针不为0）
 
+##list.h
+#ifndef _LIST_H_
+#define _LIST_H_
+
+struct Node;
+typedef struct Node *PtrToNode;
+typedef PtrToNode List;
+typedef PtrToNode Position;
+
+List MakeEmpty(List L);
+int IsEmpty(List L);
+int IsLast(Position P, List L);
+Position Find(ElementType X, List L);
+void Delete(ElementType X, List L);
+Position FindPrevious(ElementType X, List L, Position P);
+void Insert(ElementType X, List L, Position P);
+void DeleteList(List L);
+Position Header(List L);
+Position First(List L);
+Position Advance(Position P);
+ElementType Retrieve(Position P);
+
+struct Node{
+	ElementType Element;
+	Position Next;
+};
+
+#endif
+##
