56edd9bb3d5539caae83cd9cc3f96a3cbe7bccc7
CHNxish <1095219764@qq.com>
2019-06-22 16:45:38 +0800

    Update Chapter3.c

--- Basics/Data Structures and Algorithm Analysis in C:Second Edition/Chapter3.c
+++ Basics/Data Structures and Algorithm Analysis in C:Second Edition/Chapter3.c
@@ -18,3 +18,215 @@ struct Node{
   Position Next;
 };
 循环链表（A1的Prev和An的Next指针不为0）
+
+##List.h
+
+#ifndef _LIST_H_
+#define _LIST_H_
+
+struct Node;
+typedef int ElementType;
+typedef struct Node *PtrToNode;
+typedef PtrToNode List;
+typedef PtrToNode Position;
+
+List InitListNode();
+List MakeEmpty(List L);
+int IsEmpty(List L);
+int IsLast(Position P, List L);
+Position Find(ElementType X, List L);
+void Delete(ElementType X, List L);
+Position FindPrevious(ElementType X, List L, Position P);
+void Insert(ElementType X, List L, Position P);
+void DeleteList(List L);
+Position Header(List L);
+Position First(List L);
+Position Advance(Position P);
+ElementType Retrieve(Position P);
+/*
+void PrintList(const List L);
+void PrintLots(List L, int A[]);
+void SwapWithNext(Position BeforeP, List L);
+List IntersectList(List L1, List L2);
+List UnionList(Position L1, Position L2);
+void ReverseList(List L);
+*/
+
+struct Node{
+	ElementType Element;
+	Position Next;
+};
+
+#endif
+
+##
+
+##FalatError.h
+
+#ifndef _FALAT_ERROR_H_
+#define _FALAT_ERROR_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#define Error(str) FalatError(str)
+#define FalatError(str)
+fprintf(stderr,"%s\n",str),exit(1);
+
+#endif
+
+##
+
+##list.c
+
+#include <stdlib.h>
+#include "List.h"
+#include "FalatError.h"
+
+//初始化表聊节点
+List InitListNode(){
+	struct Node L;
+	L = (struct Node *)malloc(sizeof(struct Node));
+	if(L == NULL)
+		FalatError("Out of memory!");
+	L->next = NULL;
+	return L;
+}
+//如果L非空就删除原本的链表，如果是空就重新分配空间
+List MakeEmpty(List L){
+	if(L != NULL)
+		DeleteList(L);
+
+	L = InitListNode();
+	return L;	
+}
+
+int IsEmpty(List L){
+	return L->next == NULL;
+}
+
+int IsLast(Position P, List L){
+	return P->next == NULL;
+}
+
+//找到返回Position，如果没有找到返回NULL
+Position Find(ElementType X, List L){
+	Position P;
+
+	P = L->next;
+	while( P != NULL && P->Element != X )
+		P = P->next;
+	return P;
+}
+
+void Delete(ElementType X, List L){
+	Position P, TmpCell;
+
+	P = FindPrevious(X,L);
+	if(!IsLast(P,L)){
+		TmpCell = P->next;
+		P->next = TmpCell->next;
+		free(TmpCell);
+		TmpCell = NULL;	
+	}
+}
+
+Position FindPrevious(ElementType X, List L, Position P){
+	Position P;
+
+	P = L;
+	while(P->next != NULL && P->next->Element != X)
+		P = P->next;
+
+	return P;
+}
+
+void Insert(ElementType X, List L, Position P){
+	Position TmpCell;
+
+	if(P == NULL)
+		FatalError("The previous node does not exist!");
+	TmpCell = InitListNode();
+	TmpCell->Element = X;
+	TmpCell->next = P->next;
+	P->next = TmpCell;	
+}
+
+void DeleteList(List L){
+	Position P, Temp;
+	
+	P = L->next;
+	L->next =NULL;
+	while(P){
+		Temp = P->next;
+		free(P);
+		P = Temp;
+	}
+}
+
+Position Header(List L){
+	return L;
+}
+
+Position First(List L){
+	return L->next;
+}
+
+//返回P的下一个指针
+Position Advance(Position P){
+	return P->next;
+}
+
+ElementType Retrieve(Position P){
+	return P->Element;
+}
+
+##
+
+##additionallist.c
+
+#include "list.h"
+
+void PrintList(const List L){
+	Position P = Header(L);
+	if(IsEmpty(L))
+		printf("Empty List!\n");
+	else{
+		do{
+			P = Advance(P);
+			printf("%d ",Retrieve(P));
+		}while(!IsLast(P,L));
+		printf("\n");
+	}
+}
+
+//数组A以-1
+void PrintLots(List L, int A[]){
+	int i,count;
+	Position Lpos;
+
+	Lpos = First(L);
+	for(i = 0, count = 1; A[i] != -1 && Lpos != NULL; Lpos = Advance(Lpos), count++){
+		if(count == A[i]){
+			printf("%d ",Lpos->Element);
+			i++;
+		}
+	}
+}
+
+void SwapWithNext(Position BeforeP, List L){
+	
+}
+
+List IntersectList(List L1, List L2){
+
+}
+
+List UnionList(Position L1, Position L2){
+
+}
+
+void ReverseList(List L){
+
+}
+
+##
