5c5da7a491f515e9bf173b5cb17b033f990b28b3
CHNxish <1095219764@qq.com>
2019-08-05 16:25:00 +0800

    Update FatalError.h

--- Data Structure/FatalError.h
+++ Data Structure/FatalError.h
@@ -1,4 +1,10 @@
 #ifndef _FATAL_ERROR_H_
 #define _FATAL_ERROR_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#define Error(str) FatalError(str)
+#define FatalError(str) \
+fprintf(stderr, "%s", str), exit(1);
+
 #endif
