9d52829ab4e3b08f8cae5cf66dd731309972defc
CHNxish <1095219764@qq.com>
2019-08-08 22:21:00 +0800

    Update SqListADT.h

--- Data Structure/SqListADT.h
+++ Data Structure/SqListADT.h
@@ -6,4 +6,18 @@ typedef struct{
     int capacity;
     int length;
     ElementType *data;
-}
+}SqList, *PtrSqList;
+
+PtrSqList InitSqList(int maxSize);
+PtrSqList MakeEmptySqList(PtrSqList list);
+int IsEmptySqList(PtrSqList list);
+int IsLastSqList(int position, PtrSqList list);
+int Find(ElementType element, PtrSqList list);
+void DeleteNodeSqList(ElementType element, PtrSqList list);
+int FindPreviousSqList(ElementType element, PtrSqList list);
+void InsertSqList(ElementType element, PtrSqList list, int position);
+void DeleteSqList(PtrSqList list);
+int HeaderSqList()
+
+
+#endif
