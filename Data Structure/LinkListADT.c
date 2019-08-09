e1b79c7202edc5eea417fa1e616a2443b4e87d3c
CHNxish <1095219764@qq.com>
2019-08-09 11:58:02 +0800

    Update LinkListADT.c

--- Data Structure/LinkListADT.c
+++ Data Structure/LinkListADT.c
@@ -33,15 +33,10 @@ int IsLastLinkList(PtrLinkList list, PtrLinkList position){
     retutn position -> next == NULL;
 }
 
-// 
+// 找出首个包含该元素的结点
 PtrLinkList FindLinkList(ElementType element, PtrLinkList list){
-    
-}
-
-// 
-void DeleteNodeLinkList(ElementType element, PtrLinkList list){
     PtrLinkList position;
-    
+
     position = list -> next;
     while(position != NULL && position -> data != element)
         position = position -> next;
@@ -50,36 +45,66 @@ void DeleteNodeLinkList(ElementType element, PtrLinkList list){
 }
 
 // 删除首个包含该元素的结点
-PtrLinkList FindPrevious(ElementType element, PtrLinkList list){
+void DeleteNodeLinkList(ElementType element, PtrLinkList list){
+    PtrLinkList position, tmpCell;
     
+    position = FindPreviousLinkList(element, list);
+    if(!IsLastLinkList(position, list)){
+        tmpCell = position -> next;
+        position = position -> next;
+        free(tmpCell);
+    }
 }
 
+// 找出首个包含该元素的前一结点
+PtrLinkList FindPrevious(ElementType element, PtrLinkList list){
+    PtrLinkList position;
 
+    position = list;
+    while(position -> next != NULL && position -> next -> data != element)
+        position = position -> next;
+
+    return position;
+}
+
+// 在该结点后插入该元素
 void InsertLinkList(ElementType element, PtrLinkList list, PtrLinkList position){
+    PtrLinkList tmpCell;
 
+    tmpCell = InitLinkList();
+    tmpCell -> data = element;
+    tmpCell -> next = position -> next;
+    position -> next = tmpCell;
 }
 
-
+// 删除链表
 void DeleteLinkList(PtrLinkList list){
+    PtrLinkList position, tmpCell;
 
+    position = list -> next;
+    while(position != NULL){
+        tmpCell = position;
+        position = position -> next;
+        free(tmpCell);
+    }
 }
 
-
+// 返回头结点
 PtrLinkList HeaderLinkList(PtrLinkList list){
-
+    return list;
 }
 
-
+// 返回首结点
 PtrLinkList FirstLinkList(PtrLinkList list){
-
+    return list -> next;
 }
 
-
+// 返回下一结点
 PtrLinkList AdvanceLinkList(PtrLinkList position){
-
+    return position -> next;
 }
 
-
+// 返回该结点内元素
 ElementType RetrieveLinkList(PtrLinkList list, PtrLinkList position){
-
+    return position -> data;
 }
