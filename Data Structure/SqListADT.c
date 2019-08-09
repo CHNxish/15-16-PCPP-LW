a3e605247d4ee148019b1eb7e6c11e78edf1f05d
CHNxish <1095219764@qq.com>
2019-08-09 11:03:10 +0800

    Update SqListADT.c

--- Data Structure/SqListADT.c
+++ Data Structure/SqListADT.c
@@ -33,7 +33,7 @@ int IsEmptySqList(PtrSqList list){
 
 // 检查该结点是不是最后一个结点
 int IsLastSqList(int position, PtrSqList list){
-    return position == list -> capacity - 1;
+    return position == list -> length - 1;
 }
 
 // 找出首个包含该元素的结点
@@ -47,14 +47,73 @@ int FindSqList(ElementType element, PtrSqList list){
     if(position == list -> length)
         return NONE;
     else
-        return position ;
+        return position;
 }
 
-void DeleteNodeSqList(ElementType element, PtrSqList list);
-int FindPreviousSqList(ElementType element, PtrSqList list);
-void InsertSqList(ElementType element, PtrSqList list, int position);
-void DeleteSqList(PtrSqList list);
-int HeaderSqList(PtrSqList list);
-int FirstSqList(PtrSqList list);
-int AdvanceSqList(int position);
-ElementType RetrieveSqList(int position);
+// 删除首个包含该元素的结点
+void DeleteNodeSqList(ElementType element, PtrSqList list){
+    int position;
+
+    position = FindSqList(element, list);
+    if(position != NONE){
+        while(!IsLastSqList(position, list)){
+            list -> data[position] = list -> data[position + 1];
+            position++;
+        }
+        list -> length -= 1;
+    }
+}
+
+// 找出首个包含该元素的前一结点
+int FindPreviousSqList(ElementType element, PtrSqList list){
+    int position;
+
+    position = FindSqList(element, list);
+    if(position == NONE)
+        return NONE;
+    //如果position为0，也就是首结点，返回-1(即NONE)
+    else
+        return position - 1;
+}
+
+// 在该结点后插入该元素
+void InsertSqList(ElementType element, PtrSqList list, int position){
+    int tmpCell;
+
+    if(position + 1 <= list -> capacity - 1){
+        tmpCell = position + 1;
+        position = list -> length;
+        while(position > tmpCell){
+            list -> data[position] = list -> data[position - 1];
+        }
+        list -> data[tmpCell] = element;
+        list -> length += 1;
+    }
+    else
+        Error("Sequence list is full!\n");
+}
+
+// 删除顺序表
+void DeleteSqList(PtrSqList list){
+    list -> length = 0;
+}
+
+// 没有头结点，返回首结点，等同于FirstSqList
+int HeaderSqList(PtrSqList list){
+    return 0;
+}
+
+// 返回首节点
+int FirstSqList(PtrSqList list){
+    return 0;
+}
+
+// 返回下一结点
+int AdvanceSqList(int position){
+    return position + 1;
+}
+
+//返回该结点内元素
+ElementType RetrieveSqList(int position, PtrSqList list){
+    retutn list -> data[position];
+}
