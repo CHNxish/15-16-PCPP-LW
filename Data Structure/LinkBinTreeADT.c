c133b2a93fccd691da523cd88cc7d5817b5bf67c
chnxish <1095219764@qq.com>
2019-10-01 08:05:44 +0800

    Update

--- Data Structure/LinkBinTreeADT.c
+++ Data Structure/LinkBinTreeADT.c
@@ -3,7 +3,7 @@
 void PreOrderBinTree(PtrChildBinTree tree){
     PtrChildBinTree pt;
     PtrSqStack stack = CreateSqStack(SIZE);
-    
+
     if(tree != NULL){
         PushSqStack(stack, tree);
         while(stack -> top != 0){
@@ -21,7 +21,7 @@ void PreOrderBinTree(PtrChildBinTree tree){
 void InOrderBinTree(PtrChildBinTree tree){
     PtrChildBinTree pt;
     PtrSqStack stack = CreateSqStack(SIZE);
-    
+
     pt = tree;
     while(pt != NULL || !IsEmptySqStack(stack)){
         if(pt!= NULL){
@@ -41,7 +41,7 @@ void PostOrderBinTree(PtrChildBinTree tree){
     PtrChildBinTree pt;
     PtrSqStack stack = CreateSqStack(SIZE);
     int flag;
-    
+
     if(tree != NULL){
         do{
             while(tree != NULL){
@@ -66,4 +66,74 @@ void PostOrderBinTree(PtrChildBinTree tree){
     }
 }
 
+/* 以字符串的形式“根 左子树 右子树” */
+BOOL CreateBinTreeForString(PtrChildBinTree tree, int array[], int size, int n){
+    int ch;
+
+    if(n < size)
+        ch = array[n];
+    else
+        return TRUE;
+
+    if(ch == ' ') tree = NULL;
+    else{
+        tree = CreateLinkBinTree(tree);
+        tree -> data = ch;
+        return CreateBinTreeForString(tree -> lChild);
+        return CreateBinTreeForString(tree -> rChild);
+    }
+}
+
 /* 先序和中序构造树 */
+void CrtBT(PtrChildBinTree tree, char pre[], char ins[], int ps, int is, int size){
+    //pre[ps --- ps + size - 1] ins[is --- is + size - 1]
+    int k;
+
+    if(size == 0) tree = NULL;
+    else{
+        k = SearchArray(ins, SIZE, pre[ps]);
+        if(k == -1) tree = NULL;
+        else{
+            tree = CreateLinkBinTree();
+            tree -> data = pre[ps];
+            if(k == is)
+                tree -> lChild = NULL;
+            else
+                CtrBT(tree -> lChild, pre, ins, ps + 1, is, k - is);
+
+            if(k == is + size - 1)
+                tree -> rChild = NULL;
+            else
+                CtrBT(tree -> rChild, pre, ins, ps + 1 + (k - is), k + 1, n - (k - is) -1);
+        }
+    }
+}
+
+int SearchArray(char ins[], int size, char ch){
+    int i;
+
+    for(i = 0; i < size; i++){
+        if(ch == ins[i]) return i;
+    }
+    return -1;
+}
+
+/* 按给定的表达式建二叉树 */
+
+/* 进行层次遍历 */
+void LevelOrder(PtrChildBinTree tree){
+    PtrChildBinTree Queue[MAX_TREE_SIZE];
+    int front, rear;
+
+    if(tree == NULL) return;
+
+    front = -1; rear = 0;
+    Queue[rear] = b;
+    while(front != rear){
+        Visit(Queue[++front] -> data);
+        if(Queue[front] -> lChild != NULL)
+            Queue[++rear] = Queue[front] -> lChild;
+        if(Queue[front] -> rChild != NULL
+            Queue[++rear] = Queue[front] -> rChild7yyy;
+    }
+}
